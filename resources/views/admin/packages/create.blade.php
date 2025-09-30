0c7495b09accc68d2646def8a471f54d08e17694
abdallah aly <154508942+abdallahalyhassan@users.noreply.github.com>
2025-09-30 23:18:00 +0300

    second commit

--- resources/views/admin/packages/create.blade.php
+++ resources/views/admin/packages/create.blade.php
@@ -17,8 +17,6 @@
                 <div class="card">
                     <div class="card-body">
 
-
-
                         <form class="forms-sample" action="{{ route('packages.store') }}" method="POST">
                             @csrf
                             @if ($errors->any())
@@ -34,37 +32,38 @@
                             <div class="form-group">
                                 <label for="exampleInputName1">Name OF package</label>
                                 <input type="text" class="form-control" name='name' id="name"
-                                    placeholder="Name ">
+                                    placeholder="Name " value="{{ old('name') }}">
                             </div>
+
                             <div class="form-group">
                                 <label for="exampleInputName1">Description</label>
                                 <input type="text" class="form-control" name='description' id="description"
-                                    placeholder="description ">
+                                    placeholder="description " value="{{ old('description') }}">
                             </div>
+
                             <div class="form-group">
                                 <label for="exampleInputName1">Percent</label>
                                 <input type="number" max="100" min="1" class="form-control" name='percent'
-                                    id="percent" placeholder="percent">
+                                    id="percent" placeholder="percent" value="{{ old('percent') }}">
                             </div>
+
                             <div class="form-group">
                                 <label for="duration">Duration</label>
                                 <select class="form-control" name="duration" id="duration">
-                                    <option value="3">3 Months</option>
-                                    <option value="6">6 Months</option>
-                                    <option value="9">9 Months</option>
-                                    <option value="12">12 Months</option>
+                                    <option value="3" {{ old('duration') == 3 ? 'selected' : '' }}>3 Months</option>
+                                    <option value="6" {{ old('duration') == 6 ? 'selected' : '' }}>6 Months</option>
+                                    <option value="9" {{ old('duration') == 9 ? 'selected' : '' }}>9 Months</option>
+                                    <option value="12" {{ old('duration') == 12 ? 'selected' : '' }}>12 Months</option>
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="expire_at">Expire At</label>
                                 <input type="date" class="form-control" name="expire_at" id="expire_at"
-                                    min="{{ date('Y-m-d') }}">
+                                    min="{{ date('Y-m-d') }}" value="{{ old('expire_at') }}">
                             </div>
 
-
                             <button type="submit" class="btn btn-primary mr-2">Submit</button>
-
                             <a href="{{ route('packages.index') }}" class="btn btn-light">Cancel</a>
                         </form>
                     </div>
